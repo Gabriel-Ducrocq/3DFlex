26b84d293a11b3a5bc67f66e5a05cd9734147621
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-07-07 21:14:25 +0200

    again

--- comparisonNumbaJax.py
+++ comparisonNumbaJax.py
@@ -30,6 +30,12 @@ def func(i, tup):
     #return (jnp.sum(res), x,y)
 
 
+def f(x, y, i):
+    res = jnp.dot(x, y[i, :])
+    return res
+
+f_jit = jax.jit(f)
+
 
 
 
@@ -39,7 +45,26 @@ func_jit = jax.jit(func)
 
 start = time.time()
 #res, _, _ = jax.lax.fori_loop(0, 320**3, func_jit, (0, x, y))
-res = jnp.dot(x, y[0, :])
+#res = jnp.dot(x, y[0, :])
+res = f_jit(x,y,0)
+end = time.time()
+print(end-start)
+print(res.shape)
+
+
+start = time.time()
+#res, _, _ = jax.lax.fori_loop(0, 320**3, func_jit, (0, x, y))
+#res = jnp.dot(x, y[0, :])
+res = f_jit(x,y,0)
+end = time.time()
+print(end-start)
+print(res.shape)
+
+
+start = time.time()
+#res, _, _ = jax.lax.fori_loop(0, 320**3, func_jit, (0, x, y))
+#res = jnp.dot(x, y[0, :])
+res = f_jit(x,y,1)
 end = time.time()
 print(end-start)
 print(res.shape)
